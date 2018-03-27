373d3f8113cd748a06c456fff89b2643c560ed17
unknown <tpham20908@gmail.com>
2018-03-27 17:21:29 -0400

    restore MapTest

---
+++ MapTest/MapTest/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Maps.MapControl.WPF;
+
+namespace MapTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+           // MyMap.ViewChangeOnFrame += new EventHandler<MapEventArgs>(MyMap_ViewChangeOnFrame);
+
+        }
+
+
+        private void MyMap_MouseRightButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            //Gets the map that raised this event
+            Map map = (Map)sender;
+            //Gets the bounded rectangle for the current frame
+            LocationRect bounds = map.BoundingRectangle;
+            //Update the current latitude and longitude
+            currentPosition.Clear();
+            currentPosition.Text += String.Format("Northwest: {0:F5}, Southeast: {1:F5} (Current)",
+                        bounds.Northwest, bounds.Southeast);
+        }
+    }
+}
