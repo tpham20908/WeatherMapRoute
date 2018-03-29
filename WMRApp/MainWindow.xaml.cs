34067d92260f1a31b309862aabd2c33eb5da6705
unknown <tpham20908@gmail.com>
2018-03-29 17:46:40 -0400

    pushpins shows up on mouseclick

--- WMRApp/MainWindow.xaml.cs
+++ WMRApp/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Maps.MapControl.WPF;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -46,6 +47,26 @@ namespace WMRApp
         private void btnClear_Click(object sender, RoutedEventArgs e)
         {
 
+        }
+
+        private void MyMap_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            // Disables the default mouse double-click action.
+            e.Handled = true;
+
+            // Determin the location to place the pushpin at on the map.
+
+            //Get the mouse click coordinates
+            Point mousePosition = e.GetPosition(MyMap);
+            //Convert the mouse coordinates to a locatoin on the map
+            Location pinLocation = MyMap.ViewportPointToLocation(mousePosition);
+
+            // The pushpin to add to the map.
+            Pushpin pin = new Pushpin();
+            pin.Location = pinLocation;
+
+            // Adds the pushpin to the map.
+            MyMap.Children.Add(pin);
         }
     }
 }
